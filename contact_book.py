f30c89b154450040f61e2c95cd75bce2a7ca6142
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-12 14:50:02 -0500

    Create adding a contact function

--- contact_book.py
+++ contact_book.py
@@ -12,4 +12,13 @@ def display_menu():
     print("5. Display all contacts")
     print("6. Exit")
     
-#
+#Step2: Adding New Contact:
+
+def add_contact():
+    name = input("Enter the contact´s name: ")
+    email = input("Enter the contact´s email: ")
+    phone = input("Enter the contact´s phone number: ")
+    # creamos un diccionario usando la información de arriba
+    contact = {"Name": name, "Email": email, "Phone": phone}
+    contacts.append(contact)
+    print("Contact added successfully!")
