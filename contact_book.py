d2fb2417e053088278eb233e819a1d3e9d21f28e
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-12 14:46:38 -0500

    adding a display Menu

--- contact_book.py
+++ contact_book.py
@@ -2,3 +2,14 @@
 
 contacts = []
 
+#This is the menu for the Store or Contact Book app. Here you will select the action she/he wants to perform
+def display_menu():
+    print("Contact Book Menu")
+    print("1. Add a contact")
+    print("2. Search for a contact")
+    print("3. Update a contact")
+    print("4. Delete a contact")
+    print("5. Display all contacts")
+    print("6. Exit")
+    
+#
